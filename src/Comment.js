d79d97905f0873207018de11adddbef63ad29222
florapetersen <floramarina@comcast.net>
2021-01-29 12:54:37 -0800

    commit tree 536c6ba4c6545395374184a528f5a0868683b36c

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,15 @@
 //your code here
+import React, { Component } from 'react';
+
+class Comment extends Component {
+    // every class based component needs a render method // 
+    render () {
+        return (
+            <div className="comment">
+                {this.props.commentText} 
+            </div>
+        )
+    }
+}
+
+export default Comment;
